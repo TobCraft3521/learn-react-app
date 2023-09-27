bb1a7da349bed7c22c0d2c0a8cb8acefd567d9f8
TobCraft3521 <tobiashackenberg@outlook.de>
2023-09-27 20:30:58 +0200

    Bug fix of some random package and clickhandler

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
 const Home = () => {
-    return ( 
+    const handleClick = () => {
+        console.log("Super button")
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("hello", name)
+    }
+
+    return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => { handleClickAgain("TobCraft3521") }}>Click me again</button>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
