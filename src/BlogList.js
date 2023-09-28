0d07b866a3ffc319b3777ca82f4c91efbc54c221
TobCraft3521 <tobiashackenberg@outlook.de>
2023-09-28 16:51:52 +0200

    Created db

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,12 @@
-const BlogList = (props) => {
+const BlogList = ({blogs,title,handleDelete}) => {
     return (
         <div className="blog-list">
-            <h2>{props.title}</h2>
-            {props.blogs.map((blog) => (
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author} </p>
+                    <button onClick={() => handleDelete(blog.id)}>delete</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,19 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: "My new webside", body: "lorem ipsum...", author: "TobCraft3521", id: 1 },
-        { title: "My new react app", body: "lorem ipsum...", author: "ClayrCraft3521", id: 2 },
-        { title: "Ad vor my webside", body: "lorem ipsum...", author: "TobCraft3521", id: 3 },
-        { title: "Random blog", body: "lorem ipsum...", author: "user1", id: 4 },
-    ])
+    const [blogs, setBlogs] = useState(null)
+
+    useEffect(() => {
+       fetch()
+    })
+
+    const handleDelete = (id) => {
+        setBlogs(blogs.filter(blog => blog.id !== id))
+    }
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs"/>
-            <BlogList blogs = {blogs.filter((blog) => blog.author === "TobCraft3521")} title="TobCraft3521s blogs" />
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter((blog) => blog.author === "TobCraft3521")} title="TobCraft3521s blogs" handleDelete={handleDelete} />
         </div>
     );
 }
