2e45e93cda88c5d8bf074654b8905635723c7511
TobCraft3521 <tobiashackenberg@outlook.de>
2023-09-28 20:51:11 +0200

    Added custom useFetch hook

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,12 @@
 import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
+import useFetch from "./UseFetch";
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-
-    useEffect(() => {
-        fetch("http://localhost:8000/blogs").then(res => {
-            return res.json()
-        }).then(data => {
-            setBlogs(data)
-        })
-    })
-
+    const {data:blogs,error} = useFetch("http://localhost:8000/blogs")
+   
     return (
         <div className="home">
+            {error&& <div>Error: {error}</div> }
             {!blogs && <div>Loading</div> }
             {blogs && <BlogList blogs={blogs} title="All blogs"/>}
             {/*blogs && <BlogList blogs={blogs.filter((blog) => blog.author === "TobCraft3521")} title="TobCraft3521s blogs" />*/}
---
+++ src/UseFetch.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [error,setError] = useState(null)
+    useEffect(() => {
+        fetch(url).then(res => {
+            if (!res.ok) {
+                throw Error("response has a status of " + res.status)
+            }
+            return res.json()
+        }).then(data => {
+            setData(data)
+        }).catch(error => {
+            setError(error)
+            console.error(error)
+        })
+    })
+    return { data , error}
+}
+
+export default useFetch
