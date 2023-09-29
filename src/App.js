6a61dba2e53f6e82ab31bbf2e82764a3248fadb7
TobCraft3521 <tobiashackenberg@outlook.de>
2023-09-29 21:57:22 +0200

    Added dark/light mode switch and fixed bugs

--- src/App.js
+++ src/App.js
@@ -5,19 +5,54 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Create from './Create';
 import Blog from './Blog';
 import NotFound from './NotFound';
+import Cookies from 'universal-cookie';
+import { useState } from 'react';
 
 function App() {
+  const cookies = new Cookies();
+  let cssVars
+
+  const [appearence,setAppearence] = useState("light")
+
+  if (cookies.get("appearence") === "dark") {
+    cssVars = {
+      "--background": "#141414",
+      "--nav": "#1b1b1b",
+      "--text": "#ffff"
+    }
+  }
+
+  const handleAppearenceSwitch = () => {
+    if (cookies.get("appearence") === "dark") {
+      cookies.set("appearence", "light")
+      cssVars = {
+        "--background": "#fff",
+        "--nav": "#fff",
+        "--text": "#333"
+      }
+      setAppearence("light")
+    } else {
+      cookies.set("appearence", "dark")
+      cssVars = {
+        "--background": "#141414",
+        "--nav": "#1b1b1b",
+        "--text": "#ffff"
+      }
+      setAppearence("dark")
+    }
+  }
+
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
+      <div className="App" style={cssVars}>
+        <Navbar handleAppearenceSwitch={handleAppearenceSwitch} />
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create />} />
             <Route path="/about" element={<About />} />
-            <Route path='/blog/:id' element={<Blog/>} />
-            <Route path='*' element={<NotFound/>}/>
+            <Route path='/blog/:id' element={<Blog />} />
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </div>
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({handleAppearenceSwitch}) => {
     return (
         <nav className="navbar">
             <h1>Tobcrafts blog</h1>
@@ -9,6 +9,7 @@ const Navbar = () => {
                 <Link to="/about">About</Link>
                 <Link to="/create">New Blog</Link>
             </div>
+            <label><img draggable={false} src="brightness-and-contrast.png" alt="dark/light mode" onClick={handleAppearenceSwitch} width="40px" height="40px" style={{ "background": "white", "borderRadius": "100%", "marginLeft": "20px", "cursor": "pointer" }} /></label>
         </nav>
     );
 }
